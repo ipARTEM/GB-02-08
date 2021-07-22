1e646270ebbccb54411989e009070061a3e293ec
a.khimin <a.khimin@yandex.ru>
2021-07-22 16:19:02 +0300

    Добавьте файлы проекта.

---
+++ GB-02-08/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GB_02_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
